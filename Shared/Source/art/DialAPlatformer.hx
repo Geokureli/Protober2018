480e6242e6a1635659616808ffca0c656e504029
George FunBook <gkurelic@gmail.com>
2018-10-17 15:06:34 -0500

    add variable jump and coyote time

--- Shared/Source/art/DialAPlatformer.hx
+++ Shared/Source/art/DialAPlatformer.hx
@@ -6,9 +6,16 @@ import flixel.FlxObject;
 
 class DialAPlatformer extends flixel.FlxSprite { 
     
+    /** The time you can jump after walking off a cliff (ACME tm) */
+    
+    public var coyoteTime = 0.0;
+    
     var _jumpVelocity:Float;
     var _jumpUpTime:Float;
     var _moveAccel:Float;
+    var _coyoteTimer = 0.0;
+    var _jumpHoldTime = 0.0;
+    var _jumpHoldTimer = 0.0;
     
     var _keys:Map<String, Array<FlxKey>> = new Map<String, Array<FlxKey>>();
     var _keyStates:Map<String, Bool> = new Map<String, Bool>();
@@ -21,6 +28,20 @@ class DialAPlatformer extends flixel.FlxSprite {
         _jumpUpTime = jumpUpTime;
         acceleration.y = 2 * jumpHeight / jumpUpTime / jumpUpTime;
         _jumpVelocity = -2 * jumpHeight / jumpUpTime;
+        
+        if (maxFallRatio > 0)
+            maxVelocity.y = _jumpVelocity;
+        
+        setKeys([FlxKey.A, FlxKey.LEFT], [FlxKey.D, FlxKey.RIGHT], [FlxKey.SPACE, FlxKey.W, FlxKey.UP]);
+    }
+    
+    function setupVariableJump(minJumpHeight:Float, maxJumpHeight:Float, minJumpUpTime:Float, maxFallRatio:Float = 0) {
+        
+        _jumpUpTime = minJumpUpTime;
+        acceleration.y = 2 * minJumpHeight / minJumpUpTime / minJumpUpTime;
+        _jumpVelocity = -2 * minJumpHeight / minJumpUpTime;
+        
+        _jumpHoldTime = (maxJumpHeight - minJumpHeight) / -_jumpVelocity;
         
         if (maxFallRatio > 0)
             maxVelocity.y = _jumpVelocity;
@@ -64,16 +85,39 @@ class DialAPlatformer extends flixel.FlxSprite {
         
         acceleration.x = 0;
         
-        if (_keyStates['left'])
-            acceleration.x = -_moveAccel;
+        if (_keyStates['left' ]) acceleration.x = -_moveAccel;
+        if (_keyStates['right']) acceleration.x =  _moveAccel;
         
-        if (_keyStates['right'])
-            acceleration.x = _moveAccel;
+        _coyoteTimer += elapsed;
+        if (isTouching(FlxObject.FLOOR))
+            _coyoteTimer = 0;
         
-        if (_keyStates['jump'] && isTouching(FlxObject.FLOOR))
-            velocity.y = _jumpVelocity;
+        _jumpHoldTimer += elapsed;
+        if (getOnCoyoteGround())
+            _jumpHoldTimer = 0;
+        else if (!_keyStates['jump'])
+            _jumpHoldTimer = _jumpHoldTime;
+        
+        if (_keyStates['jump'] && getCanJump())
+            jump();
         
         super.update(elapsed);
+    }
+    
+    function getOnCoyoteGround():Bool {
+        
+        return _coyoteTimer < coyoteTime || isTouching(FlxObject.FLOOR);
+    }
+    
+    function getCanJump():Bool {
+        
+        return getOnCoyoteGround() || _jumpHoldTimer <= _jumpHoldTime;
+    }
+    
+    function jump() {
+        
+        velocity.y = _jumpVelocity;
+        _coyoteTimer = coyoteTime;
     }
     
     function updateKeys():Void {
