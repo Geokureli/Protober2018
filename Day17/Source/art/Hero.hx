8c996eb1be4fb090e4831b0ab61f118b44a44e0a
George FunBook <gkurelic@gmail.com>
2018-10-18 21:31:36 -0500

    testing random changes

--- Day17/Source/art/Hero.hx
+++ Day17/Source/art/Hero.hx
@@ -4,16 +4,64 @@ class Hero extends DialAPlatformer {
     
     static inline var TILE_SIZE = 8;
     
+    var _startY = 0.0;
+    
     public function new (x:Float = 0, y:Float = 0) {
         super(x, y);
         
         makeGraphic(12, 18, 0xFF6ABE30);
         
-        setupVariableJump(TILE_SIZE * 3.25, TILE_SIZE * 6.25, .35);
+        // setupVariableJump(TILE_SIZE * 3.25, TILE_SIZE * 6.25, .35);
+        setupJump(TILE_SIZE * 3.25, .35);
         setupVariableAirJump(TILE_SIZE * 1.25, TILE_SIZE * 3.25);
         setupSpeed(TILE_SIZE * 8, .25);
-        setupWallJump(TILE_SIZE * 3.25, TILE_SIZE * 3);
+        //setupWallJump(TILE_SIZE * 3.25, TILE_SIZE * 3);
+        setupSkidJump(TILE_SIZE * 7.25);
         coyoteTime = .1;
-        numAirJumps = 4;
+        jumpDirectionChange = true;
+        airJumpDirectionChange = true;
+        numAirJumps = 1;
+        inverseDrag = false;
+    }
+    
+    override function update(elapsed:Float) {
+        super.update(elapsed);
+        
+        if (velocity.y >= 0) {
+            
+            if (y + 0.5 < _startY)
+                trace('${_startY - y}');
+            
+            _startY = y;
+        }
+    }
+    
+    override function jump(justPressed:Bool) {
+        
+        super.jump(justPressed);
+        // trace('jump $justPressed $_jumpTimer < $_jumpTime');
+        
+        if(justPressed)
+            _startY = y;
+    }
+    override function airJump(justPressed:Bool) {
+        
+        super.airJump(justPressed);
+        // trace('air jump $justPressed $_jumpTimer < $_airJumpTime');
+    }
+    override function wallJump(justPressed:Bool) {
+        
+        super.wallJump(justPressed);
+        // trace('wall jump $justPressed $_jumpTimer < $_wallJumpTime');
+    }
+    override function skidJump(justPressed:Bool) {
+        
+        super.skidJump(justPressed);
+        // trace('skid jump $justPressed $_jumpTimer < $_skidJumpTime');
+    }
+    
+    override function log(msg:String) {
+        
+        trace(msg);
     }
 }
